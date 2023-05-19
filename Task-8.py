a56b1b25cb00e1b2ab9a561a2157a57f032177e8
eva <evantasiya31@gmail.com>
2023-05-19 22:38:14 +0300

    second commit to pract. 3

---
+++ Task-8.py
@@ -0,0 +1,9 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def print_info(self):
+        print(f'Имя: {self.name} Возраст:{self.age}')
+
